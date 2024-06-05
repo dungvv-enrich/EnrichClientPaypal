6848d5d19b4a54b6d7cffd071cfec4e945e0c519
dungvv <dung.vo@enrichco.us>
2024-06-05 11:46:39 +0700

    change js

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,38 @@
-import { useEffect, useState } from "react";
-import queryString from "query-string";
-import React from "react";
-import ReactDOM from "react-dom";
-import "./App.css";
+import { useEffect, useState } from "react"
+import queryString from "query-string"
+import React from "react"
+import ReactDOM from "react-dom"
+import "./App.css"
 
-let PayPalButton = null;
+let PayPalButton = null
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [amount, setAmount] = useState("");
-
+  const [isLoading, setIsLoading] = useState(true)
+  const [amount, setAmount] = useState("")
   useEffect(() => {
-    console.log("useEffect");
-    const parsedHash = queryString.parse(location.search);
-    console.log(parsedHash, " 2323");
-    setAmount(parsedHash.amount);
+    console.log("useEffect")
+    const parsedHash = queryString.parse(location.search)
+    console.log("parsedHash", parsedHash)
+    setAmount(parsedHash.amount)
     let timer2 = setInterval(() => {
-      if (sessionStorage.getItem("RVCNo") && !PayPalButton) {
-        updatePaypalButton();
-        clearInterval(timer2);
+      if (!PayPalButton) {
+        // if (sessionStorage.getItem("RVCNo") && !PayPalButton) {
+        updatePaypalButton()
+        clearInterval(timer2)
       }
-    }, 1000);
-  }, []);
+    }, 1000)
+  }, [])
 
   function updatePaypalButton() {
     try {
-      console.log("start to set PayPalButton");
+      console.log("start to set PayPalButton")
       PayPalButton = window.paypal.Buttons.driver("react", {
         React,
         ReactDOM,
-      });
-      console.log("PayPalButton", PayPalButton);
-      setIsLoading(false);
+      })
+      console.log("PayPalButton", PayPalButton)
+      setIsLoading(false)
     } catch (error) {
-      console.log("error", error);
+      console.log("error", error)
     }
   }
 
@@ -45,29 +45,29 @@ function App() {
           },
         },
       ],
-    });
+    })
   }
 
   async function _onApprove(data, actions) {
-    let order = await actions.order.capture();
-    console.log(order);
+    let order = await actions.order.capture()
+    console.log(order)
     window.ReactNativeWebView &&
-      window.ReactNativeWebView.postMessage(JSON.stringify(order));
-    return order;
+      window.ReactNativeWebView.postMessage(JSON.stringify(order))
+    return order
   }
 
   function _onError(err) {
-    console.log(err);
+    console.log(err)
     let errObj = {
       err: err,
       status: "FAILED",
-    };
+    }
     window.ReactNativeWebView &&
-      window.ReactNativeWebView.postMessage(JSON.stringify(errObj));
+      window.ReactNativeWebView.postMessage(JSON.stringify(errObj))
   }
 
   return (
-    <div className="App">
+    <div className='App'>
       {isLoading && !PayPalButton ? (
         <p>Loading...</p>
       ) : (
@@ -75,11 +75,11 @@ function App() {
           createOrder={(data, actions) => _createOrder(data, actions)}
           onApprove={(data, actions) => _onApprove(data, actions)}
           onCancel={() => _onError("Canceled")}
-          onError={(err) => _onError(err)}
+          onError={err => _onError(err)}
         />
       )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
